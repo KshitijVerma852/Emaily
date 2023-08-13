43f54a958cc7436808a668d0a3f46747de3035eb
Kshitij <kshitijverma852@gmail.com>
2023-08-13 13:48:22 +0100

    Add mongoose and save dummy data to db

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+// noinspection JSUnresolvedFunction
+
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+require("./models/User");
 require("./services/passport");
+
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 require("./routes/authRoutes")(app);
