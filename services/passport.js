1c6e22cc268071e2a8c82c59d20aef3a58fee222
Kshitij <kshitijverma852@gmail.com>
2023-08-15 21:08:21 +0100

    add login and logout functionality

--- services/passport.js
+++ services/passport.js
@@ -4,6 +4,16 @@ const { Strategy: GoogleStrategy } = require("passport-google-oauth20");
 const keys = require("../config/keys");
 const User = mongoose.model("users");
 
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id).then(user => {
+		done(null, user);
+	});
+});
+
 passport.use(
 	new GoogleStrategy(
 		{
@@ -12,9 +22,13 @@ passport.use(
 			callbackURL: "/auth/google/callback"
 		},
 		(accessToken, refreshToken, profile, done) => {
-			new User({
-				googleId: profile.id
-			}).save();
+			User.findOne({ googleId: profile.id }).then(existingUser => {
+				!existingUser
+					? new User({ googleId: profile.id })
+							.save()
+							.then(user => done(null, user))
+					: done(null, existingUser);
+			});
 		}
 	)
 );
