2273201ad39bd1082a9bb090bb9c31fda2899bb2
Kshitij <kshitijverma852@gmail.com>
2023-08-22 11:28:32 +0100

    setup redux flow

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-export default class App extends React.Component {
+export default class App extends React.Component{
 	render() {
 		return <div>Hello world</div>;
 	}
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,16 @@
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 import App from "./components/App";
+import reducers from "./reducers/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+const store = createStore(reducers, {}, applyMiddleware());
+
+root.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	root
+);
