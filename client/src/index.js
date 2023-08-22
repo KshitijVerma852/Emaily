11a04a26c78b91f7c9780c1050234036d5c19b21
Kshitij <kshitijverma852@gmail.com>
2023-08-22 14:04:35 +0100

    make basic header and start auth with redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,19 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
 import App from "./components/App";
 import reducers from "./reducers/index";
+import "materialize-css/dist/css/materialize.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const store = createStore(reducers, {}, applyMiddleware());
+
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 root.render(
 	<Provider store={store}>
 		<App />
-	</Provider>,
-	root
+	</Provider>
 );
