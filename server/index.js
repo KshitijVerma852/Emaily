7539e2b5a9c7a8c711e6eb07581102851b5bad10
kv45 <kshitijverma852@gmail.com>
2024-05-04 06:16:51 +0100

    add auth sessions

--- server/index.js
+++ server/index.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
 
-mongoose.connect(require("./config/keys").mongoURI)
+mongoose.connect(keys.mongoURI)
 	.then(() => {
 		console.log("Connected to mongo!");
 	});
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey]
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -4,6 +4,15 @@ const { Strategy: GoogleStrategy } = require("passport-google-oauth20");
 const keys = require("../config/keys");
 const User = mongoose.model("users");
 
+passport.serializeUser(({ id }, done) => {
+	done(null, id);
+});
+
+passport.deserializeUser(async (id, done) => {
+	const user = await User.findById(id);
+	done(null, user);
+});
+
 passport.use(
 	new GoogleStrategy(
 		{
