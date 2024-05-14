f9f4760e8b62af5f87770f0a227450b68840087f
kv45 <kshitijverma852@gmail.com>
2024-05-14 16:23:44 +0100

    finish the Header component

--- server/client/src/components/Header.jsx
+++ server/client/src/components/Header.jsx
@@ -1,17 +1,41 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends React.Component {
+	renderContent() {
+		switch (this.props.auth) {
+			case null:
+				return;
+			case false:
+				return (
+					<li>
+						<a href="/auth/google">Login with Google</a>
+					</li>
+				);
+			default:
+				return (
+					<li>
+						<a href="/api/logout">Logout</a>
+					</li>
+				);
+		}
+	}
+
 	render() {
+		console.log(this.props.auth);
 		return (
 			<nav>
 				<div className="nav-wrapper">
-					<a href="#" className="brand-logo" style={{paddingLeft:"10px"}}>
+					<Link
+						to={this.props.auth ? "/surveys" : "/"}
+						className="brand-logo"
+						style={{ paddingLeft: "10px" }}
+					>
 						Emaily
-					</a>
+					</Link>
 					<ul id="nav-mobile" className="right hide-on-med-and-down">
-						<li>
-							<a href="#">Login with Google</a>
-						</li>
+						{this.renderContent()}
 					</ul>
 				</div>
 			</nav>
@@ -19,4 +43,6 @@ class Header extends React.Component {
 	}
 }
 
-export default Header;
+const mapStateToProps = ({ auth }) => ({ auth: auth });
+
+export default connect(mapStateToProps)(Header);
