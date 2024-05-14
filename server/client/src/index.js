5505926887741001c8c3a31f5fef1273068046b3
kv45 <kshitijverma852@gmail.com>
2024-05-14 13:24:36 +0100

    create fetchUser action

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import "materialize-css/dist/css/materialize.min.css";
 import { Provider } from "react-redux";
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk";
 import reducers from "./reducers";
 
 import App from "./components/App";
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
